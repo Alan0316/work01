623f5e68844393bfc48c66ec15ef77b2022c7019
Alan0316 <161661534+Alan0316@users.noreply.github.com>
2024-02-29 09:18:19 +0800

    Add files via upload

---
+++ untitled4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 08:59:15 2024
+
+@author: Student
+"""
+
+| P | Q | P ∧ Q | P ∨ Q | P → Q | P ← Q | P ↔ Q |
+|---|---|-------|-------|-------|-------|-------|
+| F | F |   F   |   F   |   T   |   T   |   T   |
+| F | T |   F   |   T   |   T   |   F   |   F   |
+| T | F |   F   |   T   |   F   |   T   |   F   |
+| T | T |   T   |   T   |   T   |   T   |   T   |
